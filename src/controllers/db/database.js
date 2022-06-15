ae85cb16c4a059475a1cdfe14b056f8347345f28
Starley Cazorla <starley@artnew.com.br>
2022-06-15 16:24:53 -0300

    :art: Ao iniciar qualquer login ira cirar as pastas definidas

--- src/controllers/db/database.js
+++ src/controllers/db/database.js
@@ -7,8 +7,14 @@ let lastDbInUse = '';
 
 function checkDbInUse(nomeDb) {
 
-  if (lastDbInUse != nomeDb) {
-    return dbInUse = new sqlite3.Database(nomeDb, (err) => {
+  if (lastDbInUse != mySubString) {
+
+    var mySubString = nomeDb.substring(
+      nomeDb.indexOf("") + 0,
+      nomeDb.lastIndexOf("_nxsinter")
+    );
+
+    return dbInUse = new sqlite3.Database(`arquivos_${mySubString}/database/${nomeDb}`, (err) => {
       if (err) {
         // Cannot open database
         console.error(err.message)
--- src/controllers/db/db-controller/createDb-controller.js
+++ src/controllers/db/db-controller/createDb-controller.js
@@ -1,6 +1,8 @@
 var sqlite3 = require('sqlite3').verbose();
 var md5 = require('md5');
 
+const externoUtil = require('../../../util/folders.util');
+
 // Configuração Controller Generico
 /**
  * @author Starley Cazorla
@@ -20,7 +22,19 @@ exports.post = async (req, res, next) => {
             console.log('Sql Recebida /createDb ---> ', sqlRecebida)
             const DBSOURCE = sqlRecebida;
 
-            new sqlite3.Database(DBSOURCE, (err) => {
+            var mySubString = sqlRecebida.substring(
+                sqlRecebida.indexOf("") + 0,
+                sqlRecebida.lastIndexOf("_nxsinter")
+            );
+
+            /** Criando pastas locais */
+            externoUtil.criarPasta(`arquivos_${mySubString}/imgAws`);
+            externoUtil.criarPasta(`arquivos_${mySubString}/database`);
+            externoUtil.criarPasta(`arquivos_${mySubString}/backup`);
+            externoUtil.criarPasta(`arquivos_${mySubString}/relatorios`);
+            externoUtil.criarPasta(`arquivos_${mySubString}/download`);
+
+            new sqlite3.Database(`arquivos_${mySubString}/database/${DBSOURCE}`, (err) => {
                 if (err) {
                     // Cannot open database
                     console.error(err.message)
