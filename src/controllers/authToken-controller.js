509f6716a91a1affd0eddbdb79b5dfa47fb7b40d
Starley Cazorla <starlleycom@gmail.com>
2022-07-04 11:39:30 -0300

    :art: Adiciona dupla autenticacao para retornar token

--- src/controllers/authToken-controller.js
+++ src/controllers/authToken-controller.js
@@ -5,6 +5,7 @@
 'use sctict'
 
 const { getToken } = require('../services/jwt-service.js');
+const enviroment = require('./../config/enviroment.js');
 
 exports.post = async (req, res, next) => {
     let vendedorLogado;
@@ -15,14 +16,21 @@ exports.post = async (req, res, next) => {
     }).on('end', async function () {
 
         let data = Buffer.concat(chunks);
-        vendedorLogado = JSON.parse(data).vendedorLogado
-        console.log('vendedorLogado ---> ', vendedorLogado)
-        getToken(vendedorLogado).then(retorno => {
-            console.log("🚀 ~ file: authToken-controller.js ~ line 21 ~ getToken ~ retorno", retorno)
-            res.send([{ token: retorno }]);
-        }).catch(error => {
-            res.send({ message: `Não pegar o token! ${error}`, retorno: false });
-        })
+        vendedorLogado = JSON.parse(data).vendedorLogado;
+        chaveSecreta = JSON.parse(data).chaveSecreta;
+
+        /** Se não tiver a chave secreta corespondente não ira retornar o token */
+        if (chaveSecreta === enviroment.SECRET) {
+            console.log('vendedorLogado ---> ', vendedorLogado)
+            getToken(vendedorLogado).then(retorno => {
+                console.log("🚀 ~ file: authToken-controller.js ~ line 21 ~ getToken ~ retorno", retorno)
+                res.send([{ token: retorno }]);
+            }).catch(error => {
+                res.send({ message: `Não pegar o token! ${error}`, retorno: false });
+            })
+        } else {
+            res.status(400).send({ message: `Está faltando o segredo do cadeado! Ou a chave esta errada! :( )`, retorno: false });
+        }
 
     });
 
